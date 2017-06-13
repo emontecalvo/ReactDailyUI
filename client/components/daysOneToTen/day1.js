83e6ff7bc0439f1c65cf6f9fddd0594f29e748ec
emontecalvo <emilymontecalvo@gmail.com>
2017-06-13 14:49:46 -0700

    Day001 - added icons and input transition

--- client/components/daysOneToTen/day1.js
+++ client/components/daysOneToTen/day1.js
@@ -16,11 +16,11 @@ class Day1 extends React.Component {
           <div className="input">
           	<form onSubmit={(e) => {
           		e.preventDefault() }}>
-              <input type="text" placeholder="name" name="name" />
-          		<input type="email" placeholder="email" name="email" />
-          		<input type="password" placeholder="password" />
-          		<button type="submit">sign me up!</button>
-              <button className="Day1Btn" onClick={() => this.showHome()}>back to all UI's</button>
+              <div className="day1input"><i className="fa fa-user"></i><input type="text" placeholder="Lulu Esquire" name="name" /></div>
+          		<div className="day1input"><i className="fa fa-envelope"></i><input type="email" placeholder="lesquire@example.com" name="email" /></div>
+          		<div className="day1input"><i className="fa fa-lock"></i><input type="password" placeholder="********" /></div>
+              <button className="Day1Btn" type="submit">&nbsp;&nbsp;sign me up! <i className="fa fa-fw fa-chevron-right"></i></button>
+              <button className="Day1Btn" onClick={() => this.showHome()}><i className="fa fa-fw fa-chevron-left"></i> back to UI's&nbsp;&nbsp;&nbsp;&nbsp;</button>
           	</form>
           </div>
 
